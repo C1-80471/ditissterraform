ff75f0f6c67a1f2fef8bc728f39b2bb892897f4a
C1-80471 <pravinsawant9869@gmail.com>
2024-02-18 20:59:28 +0530

    test

--- object.tf
+++ object.tf
@@ -1,47 +1,57 @@
 resource "aws_s3_object" "index"{
-	bucket = "c180471-ditiss-project"
+	bucket = "c180471-ditiss-project2"
 	key = "index.html"
 	source = "index.html"
 	acl = "private"
 	content_type = "text/html"
+	server_side_encryption = "AES256"
+
 
 }
 
 resource "aws_s3_object" "error" {
-	bucket = "c180471-ditiss-project"
+	bucket = "c180471-ditiss-project2"
 	key = "error.html"
 	source = "error.html"
 	acl = "private"
 	content_type = "text/html"
+	server_side_encryption = "AES256"
+	
 }
 
 resource "aws_s3_object" "image" {
-	bucket = "c180471-ditiss-project"
+	bucket = "c180471-ditiss-project2"
 	key = "shinichi.jpeg"
 	source = "shinichi.jpeg"
 	acl = "private"
 	content_type = "image/jpeg"
+	server_side_encryption = "AES256"
+
 }
 
 resource "aws_s3_object" "style" {
-	bucket = "c180471-ditiss-project"
+	bucket = "c180471-ditiss-project2"
 	key = "style.css"
 	source = "style.css"
 	acl = "private"
 	content_type = "text/css"
+	server_side_encryption = "AES256"
+
 }
 
 
 resource "aws_s3_object" "script" {
-	bucket = "c180471-ditiss-project"
+	bucket = "c180471-ditiss-project2"
 	key = "script.js"
 	source = "script.js"
 	acl = "private"
 	content_type = "text/javascript"
+	server_side_encryption = "AES256"
+
 }
 
 resource "aws_s3_bucket_website_configuration" "website" {
-	bucket = "c180471-ditiss-project"
+	bucket = "c180471-ditiss-project2"
 	index_document {
 		suffix = "index.html"
 		}
